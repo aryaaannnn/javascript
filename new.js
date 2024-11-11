b6c4bece36893b7c7fb49a6f144dd50c0bf387b6
aryaaannnn <143686575+aryaaannnn@users.noreply.github.com>
2024-11-11 04:42:46 +0000

    container config

---
+++ new.js
@@ -0,0 +1,90 @@
+
+  // let userinput = prompt("enter the hours");
+  // let hours = 11;
+  // if (hours >= 5 && hours <= 12) {
+  //   alert('Good Morning');
+  // }
+  // else if (hours >= 13 && hours <= 19) {
+  //   alert('Good afternoon');
+  // }
+  // else {
+  //   alert('Good Night');
+  // }
+
+  // even or odd
+  // let num = prompt('enter your $num');
+
+  // if (num % 2 === 0) {
+  //   console.log('its a even number');
+  // } else {
+  //   console.log('its a odd numer')
+  // };
+
+  // Create a program that asks the user to input their exam score (0-100). Use if-else statements to assign a grade:
+
+  // 90-100: A
+  // 80-89: B
+  // 70-79: C
+  // 60-69: D101
+  // Below 60: F
+
+  // let grade = prompt('enter the grade');
+  // if (grade >= 90 && grade <= 100) {
+  //   console.log('he/she Secured A grade');
+  // } else if (grade >= 80 && grade <= 89) {
+  //   console.log('he/she secured B grade');
+  // } else if (grade >= 70 && grade <= 79) {
+  //   console.log('he/she secured C grade');
+  // } else if (grade >= 60 && grade <= 69) {
+  //   console.log('he/she secured D grade');
+  // } else {
+  //   console.log('Bc phr se exam de')
+  // }
+
+  //   3. Leap Year Checker
+  // Write a program that asks the user for a year and checks if it is a leap year. A year is a leap year if:
+
+  // It is divisible by 4, and
+  // It is not divisible by 100 unless also divisible by 400.
+
+  // let year = 2031;
+
+  // if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+  //   console.log('its a leap year ');
+  // } else {
+  //   console.log('its not a leap year');
+  // }
+
+  //   4. Age Category
+  // Ask the user to input their age, and use if-else if statements to determine which age group they fall into:
+
+  // 0-12: Child
+  // 13-19: Teenager
+  // 20-59: Adult
+  // 60 and above: Senior
+
+  // age = prompt('Enter your age');
+
+  // if (age >= 0 && age <= 12) {
+  //   console.log('yes! u are a child');
+  // } else if (age >= 13 && age <= 19) {
+  //   console.log('yes! u are a Teenager');
+  // } else if (age >= 20 && age <= 59) {
+  //   console.log('Yes! you a are adult');
+  // } else {
+  //   console.log('Yes! You are a Senior persion')
+  // }
+
+//   function iseven(a) {
+//     return a % 2 === 0 ? true : false
+//   }
+//   let userInput = prompt("Enter your number");
+//   userInput = parseInt(userInput);
+//   console.log(iseven(userInput));
+
+  function userlogin(username) {
+    return `${username} just login in `
+  }
+  console.log(userlogin('aryan'))
+
+
